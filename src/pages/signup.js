dbc7c234a5a72e3fe3c277a9e67fa93e9bec6ff2
Medor <alexandre.medor@gmail.com>
2023-02-11 22:29:49 +0100

    message

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ScrollToTop from "../components/ScrollToTop";
-import SignUp from "../components/Signup";
+// import SignUp from "../components/Signup";
 
 const SignupPage = () => {
   return (
